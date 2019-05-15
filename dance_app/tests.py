cd5a98c40a7319f3f6987b4646620822c9cebd6e
Benny Lee <benny.lee@msn.com>
2019-05-15 03:50:58 +0000

    remove /manage.py and media/ in gitignore, create new folder staticfiles and added empty files in it and media and static

--- dance_app/tests.py
+++ dance_app/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
-from .models import DanceGenre
+# from .models import DanceGenre
 
 # Create your tests here.
-# class DanceGenreTest(TestCase):
+class DanceGenreTest(TestCase):
     
-#     def testCanCreateDanceGenre(self):
-#         a = 
+    def testCanCreateDanceGenre(self):
+        a = DanceGenre(name="House")
+        a.save()
+        
+        b = DanceGenre.objects.all().get(pk=a.id)
+        print(b)
+        self.assertEquals(a.name, b.name)
+        self.assertEquals(a.id, b.id)
